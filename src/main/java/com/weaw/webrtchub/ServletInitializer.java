789dccdff07cd7137d1174a858d738f0a0b27612
Tom DESORMEAUX <tom.desormeauxdelaunay@gmail.com>
2024-04-25 13:52:10 +0200

    wip

---
+++ src/main/java/com/weaw/webrtchub/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.weaw.webrtchub;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebrtcHubApplication.class);
+    }
+
+}
